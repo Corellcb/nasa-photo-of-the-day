78fc92ec7e36ba000e6c2cae5dfda7ebac056831
CorellCB <corellcb@gmail.com>
2019-12-12 22:05:19 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import Header from "./components/Header";
+import PreviousImg from "./components/PreviousImg";
+import styled from "styled-components";
+
+const MainImg = styled.img`
+  width: 100%;
+`
 
 function App() {
   const [img, setimg] = useState([]);
 
   useEffect(() => {
     axios
-    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .get('https://api.nasa.gov/planetary/apod?api_key=a4pkiWLShGq24Hzq4PUekUoavqx0kCXgaOXoT51N')
     .then(response => {
       setimg(response.data);
-      console.log(response.data);
     })
     .catch(response => {
       console.log(response);
@@ -18,14 +24,16 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <img src={img.hdurl} />
+      <Header />
+      <MainImg src={img.hdurl} alt="NASA Photo of the Day"/>
       <div className="info">
-        <h1>NASA APOD</h1>
+        <h1>NASA Astronomy Picture of the Day</h1>
         <h3>{img.title}</h3>
         <p>
           {img.explanation}
         </p>
       </div>
+      <PreviousImg />
     </div>
   );
 }
